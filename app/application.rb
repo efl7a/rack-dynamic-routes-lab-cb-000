39e7ebd9f6266743a81f5c87c9940c3f3ef45134
Heather Christman <efl7a@yahoo.de>
2018-01-08 23:04:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,17 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      if @@item.include?(item)
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+    else
+      resp.write "Page not found"
+      resp.status = 404
+    end
     resp.finish
   end
 end
